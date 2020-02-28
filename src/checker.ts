c02dc07cc6581d03ab0d8f1abe457680c92d4be6
Jan Ladleif <Jan.Ladleif@hpi.uni-potsdam.de>
2020-02-28 12:56:56 +0100

    output log to a file

--- src/checker.ts
+++ src/checker.ts
@@ -24,32 +24,38 @@ export async function checkModel(xml: string, term: string): Promise<string> {
     console.log(id, 'TLA written to file');
 
     // now the interesting part: running TLC
+    let buffer: Buffer;
     try {
-      let output : Buffer;
-      output = child.execSync('java -classpath tla2tools.jar tlc2.TLC -deadlock -nowarning Choreography', { cwd: folder });
-      console.log(id, 'TLC finished with no error', output.toString());
-      return output.toString();
+      buffer = child.execSync('java -classpath tla2tools.jar tlc2.TLC -deadlock -nowarning Choreography', { cwd: folder });
     } catch (error) {
-      // trace output
-      //TODO parse
-      const trace = Buffer.concat([error.stdout, error.stderr]);
-      console.log(id, 'TLC finished with an error', trace.toString());
-      return trace.toString();
-    } finally {
-      // clean up
-      if (!KEEP_ARTIFACTS) {
-        fs.remove(folder).then(() => {
-          console.log(id, 'Cleaned up folder', folder);
-        }).catch(error => {
-          console.error(id, 'Error cleaning up', error);
-        })
-      } else {
-        fs.remove(folder + '/tla2tools.jar').then(() => {
-          console.log(id, 'Removed tla2tools.jar', folder);
-        }).catch(error => {
-          console.error(id, 'Error removing tla2tools.jar', error);
-        })
-      }
+      buffer = Buffer.concat([error.stdout, error.stderr]);
     }
+    const output = buffer.toString();
+
+    // clean up
+    if (!KEEP_ARTIFACTS) {
+      fs.remove(folder).then(() => {
+        console.log(id, 'Cleaned up folder', folder);
+      }).catch(error => {
+        console.error(id, 'Error cleaning up', error);
+      })
+    } else {
+      fs.remove(folder + '/tla2tools.jar').then(() => {
+        console.log(id, 'Removed tla2tools.jar', folder);
+      }).catch(error => {
+        console.error(id, 'Error removing tla2tools.jar', error);
+      })
+
+      // log output to a file
+      fs.outputFile(folder + '/output.log', output).then(() => {
+        console.log(id, 'Wrote log to file');
+      }).catch(error => {
+        console.error(id, 'Error cleaning up', error);
+      });
+    }
+
+    // output proper string
+    console.log(id, 'TLC finished', output);
+    return output;
   });
 };
